8450f54dad9d88038ba42914f0f173b42e5ad4bf
Theobasallaje <theobasallaje@gmail.com>
2020-11-27 00:27:51 -0600

    fixed images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import "./App.css";
-import Movies from "./movies.js";
-import image from "./assets/3_10_To_Yuma.jpg";
+import data from "./movies";
+// import image from "./assets/3_10_To_Yuma.jpg";
 
 function App() {
-  console.log(Movies);
+  console.log(data);
   return (
-    <div className="movieContainer">
-      {/* <img src={Movies.Three_Ten_To_Yuma} alt='3:10 To Yuma' /> */}
-      {/* <img src={image} alt='3:10 To Yuma' /> */}
-      <img src={require('./assets/13_Hours.jpg')} alt='3:10 To Yuma' />
-      {Movies.map(({ id, src, title, description }) => (
-        <img key={id} src={src} title={title} alt={description} />
-      ))}
-    </div>
+    <>
+      <div className="movieContainer">
+        {/* <img src={Movies.Three_Ten_To_Yuma} alt='3:10 To Yuma' /> */}
+        {/* <img src={image} alt='3:10 To Yuma' /> */}
+        {/* <img src={require('./assets/13_Hours.jpg')} alt='3:10 To Yuma' /> */}
+        {/* <img src={'./assets/13_Hours.jpg'} alt='3:10 To Yuma' /> */}
+        {/* <img src={`${process.env.PUBLIC_URL}/assets/images/uc-white.png`} alt='3:10 To Yuma' /> */}
+        {/* {data.Movies.map(
+          ({ movie, i }) => console.log(movie)
+          // <img key={movie.id} src={movie.src} title={movie.title} alt={movie.description} />
+        )} */}
+        {data.Movies.map((movie, i) => {
+          return (
+            <div key={i}>
+              <img src={`${process.env.PUBLIC_URL}/assets/images/${movie.src}`} alt={movie.description} />
+            </div>
+          );
+        })}
+      </div>
+      {/* <div>
+        {data.Experiences.map((experience, i) => {
+          return (
+            <div key={i}>
+              <div>
+                <a href={experience.url}>
+                  <img src={experience.logo} alt={experience.companyName} />
+                </a>
+                <div>
+                  <div>
+                    <a href={experience.url}>{experience.companyName}</a>
+                  </div>
+                  {experience.roles.map(function (role, i) {
+                    return (
+                      <div key={i}>
+                        <h5>{role.title}</h5>
+                        <span>{role.startDate}</span>
+                        <span>{role.location}</span>
+                        <p>{role.description}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div> */}
+    </>
   );
 }
 
